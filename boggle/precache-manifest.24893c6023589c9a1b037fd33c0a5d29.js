9ecda42616917e823d6f9150fdb6f2eb25b8fc2a
Jildert Venema <jildert.venema@hotmail.com>
2019-04-02 21:13:39 +0200

    boggle

---
+++ boggle/precache-manifest.24893c6023589c9a1b037fd33c0a5d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b6f2b6c7b529166c30",
+    "url": "/boggle/static/js/runtime~main.72847329.js"
+  },
+  {
+    "revision": "2da9c1edd18a4e748b43",
+    "url": "/boggle/static/js/main.797ea868.chunk.js"
+  },
+  {
+    "revision": "a64884c76df6934a751c",
+    "url": "/boggle/static/js/2.c9201a2e.chunk.js"
+  },
+  {
+    "revision": "2da9c1edd18a4e748b43",
+    "url": "/boggle/static/css/main.e56f1569.chunk.css"
+  },
+  {
+    "revision": "c9498297a53bc1c6b63db593775a3f7c",
+    "url": "/boggle/index.html"
+  }
+];
--- boggle/service-worker.js
+++ boggle/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/boggle/precache-manifest.64d25b1bedcdeb98297b43a93b3d670a.js"
+  "/boggle/precache-manifest.24893c6023589c9a1b037fd33c0a5d29.js"
 );
 
 workbox.clientsClaim();
