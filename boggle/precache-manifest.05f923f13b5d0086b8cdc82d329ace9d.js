428a40609fbc729760dc78eb3d82b1960a5db7d4
Jildert Venema <jildert.venema@hotmail.com>
2019-04-10 20:10:40 +0200

    dasd

---
+++ boggle/precache-manifest.05f923f13b5d0086b8cdc82d329ace9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b6f2b6c7b529166c30",
+    "url": "/boggle/static/js/runtime~main.72847329.js"
+  },
+  {
+    "revision": "565158a307d41a5f166a",
+    "url": "/boggle/static/js/main.8052af4e.chunk.js"
+  },
+  {
+    "revision": "981c65d905abe7539cbf",
+    "url": "/boggle/static/js/2.7dd384c7.chunk.js"
+  },
+  {
+    "revision": "565158a307d41a5f166a",
+    "url": "/boggle/static/css/main.2fb4d0e2.chunk.css"
+  },
+  {
+    "revision": "11ed92bae4d93d9c9837a780819de923",
+    "url": "/boggle/index.html"
+  }
+];
