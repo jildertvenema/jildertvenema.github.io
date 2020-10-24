7594ae75d795288be8d04ce12ddf3e2d63dec6de
Jildert Venema <jildert.venema@hotmail.com>
2020-10-24 15:03:21 +0200

    portfolio

---
+++ precache-manifest.bd6f597440f9a44ae154f2335a49a207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc295784a06b6a66d1dd",
+    "url": "/static/css/main.af2ee9d2.chunk.css"
+  },
+  {
+    "revision": "fc295784a06b6a66d1dd",
+    "url": "/static/js/main.fc295784.chunk.js"
+  },
+  {
+    "revision": "63fb5b41de279ed09cdd",
+    "url": "/static/js/1.63fb5b41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63b131b669fbb37f1fab1f64d6cce39d",
+    "url": "/index.html"
+  }
+];
